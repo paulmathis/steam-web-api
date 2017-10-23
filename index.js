7178c6550485094aa066fa7484b086f9e82bf320
Paul Mathis <pmathis@abor.com>
2017-10-23 17:00:56 -0500

    started changes on taking in params

--- index.js
+++ index.js
@@ -4,16 +4,19 @@
 const { truthy, apiRequest } = require('./helpers');
 
 class SteamApi {
-  constructor(apikey) {
+  constructor(apikey, steamid, appid) {
     this.apikey = apikey;
+    this.steamid = steamid;
+    this.appid = appid;
   }
 
-  getNewsForApp(appid, options = {}) {
-    return apiRequest('http://api.steampowered.com/ISteamNews/GetNewsForApp/v0002', {
-      appid,
-      count: options.count || '3',
-      maxlength: options.maxlength || '300'
-    });
+  getNewsForApp(appid, config = {}) {
+    const params = {
+      appid: config.appid || this.appid,
+      count: config.count || '3',
+      maxlength: config.maxlength || '300'
+    };
+    return apiRequest('http://api.steampowered.com/ISteamNews/GetNewsForApp/v0002', params);
   }
 
   getGlobalAchievementPercentagesForApp(appid) {
@@ -22,7 +25,7 @@ class SteamApi {
     });
   }
 
-  getGlobalStatsForGame(appid, names, options = {}) {
+  getGlobalStatsForGame(appid, names, config = {}) {
     // Takes in a string or an array and assigns the values to objects named name[0], name[1], etc...
     const namesObject = {};
     if (typeof names === 'string') {
@@ -32,35 +35,35 @@ class SteamApi {
         namesObject[`name[${i}]`] = name;
       });
     }
-    // Join the namesObject and the options into a single object that is passed as parameters
+    // Join the namesObject and the config into a single object that is passed as parameters
     return apiRequest(
       'http://api.steampowered.com/ISteamUserStats/GetGlobalStatsForGame/v0001',
       Object.assign(
         {
           appid,
-          count: options.count || '1'
+          count: config.count || '1'
         },
         namesObject
       )
     );
   }
 
-  getPlayerSummaries(steamids) {
+  getPlayerSummaries(steamids = this.steamid) {
     return apiRequest('http://api.steampowered.com/ISteamUser/GetPlayerSummaries/v002', {
       key: this.apikey,
       steamids: `${steamids}`
     });
   }
 
-  getFriendList(steamid, options = {}) {
+  getFriendList(steamid = this.steamid, config = {}) {
     return apiRequest('http://api.steampowered.com/ISteamUser/GetFriendList/v0001', {
       key: this.apikey,
       steamid,
-      relationship: options.relationship || 'friend'
+      relationship: config.relationship || 'friend'
     });
   }
 
-  getPlayerAchievements(steamid, appid, options = {}) {
+  getPlayerAchievements(steamid = this.steamid, appid, config = {}) {
     return apiRequest('http://api.steampowered.com/ISteamUserStats/GetPlayerAchievements/v0001', {
       key: this.apikey,
       steamid,
@@ -68,7 +71,7 @@ class SteamApi {
     });
   }
 
-  getUserStatsForGame(steamid) {
+  getUserStatsForGame(steamid = this.steamid) {
     return apiRequest(' http://api.steampowered.com/ISteamUserStats/GetUserStatsForGame/v0002', {
       key: this.apikey,
       steamid,
@@ -76,29 +79,33 @@ class SteamApi {
     });
   }
 
-  getOwnedGames(steamid, options = {}) {
-    return apiRequest('http://api.steampowered.com/IPlayerService/GetOwnedGames/v0001', {
+  getOwnedGames(config = {}) {
+    const params = {
       key: this.apikey,
-      steamid,
-      include_appinfo: truthy(options.include_appinfo) || false,
-      include_played_free_games: truthy(options.include_played_free_games) || false
-    });
+      steamid: config.steamid || this.steamid,
+      include_appinfo: truthy(config.include_appinfo) || false,
+      include_played_free_games: truthy(config.include_played_free_games) || false
+    };
+
+    return apiRequest('http://api.steampowered.com/IPlayerService/GetOwnedGames/v0001', params);
   }
 
-  getRecentlyPlayedGames(steamid, options = {}) {
+  getRecentlyPlayedGames(steamid = this.steamid, config = {}) {
     return apiRequest('http://api.steampowered.com/IPlayerService/GetRecentlyPlayedGames/v0001', {
       key: this.apikey,
       steamid,
-      count: options.count
+      count: config.count
     });
   }
 
-  isPlayingSharedGame(steamid, appid_playing) {
-    return apiRequest('http://api.steampowered.com/IPlayerService/IsPlayingSharedGame/v0001', {
+  isPlayingSharedGame(config = {}) {
+    const params = {
       key: this.apikey,
-      steamid,
-      appid_playing
-    });
+      steamid: config.steamid || this.steamid,
+      appid_playing: config.appid_playing
+    };
+
+    return apiRequest('http://api.steampowered.com/IPlayerService/IsPlayingSharedGame/v0001', params);
   }
 
   getSchemaForGame(appid) {
@@ -108,7 +115,7 @@ class SteamApi {
     });
   }
 
-  getPlayerBans(steamids) {
+  getPlayerBans(steamids = this.steamid) {
     return apiRequest('http://api.steampowered.com/ISteamUser/GetPlayerBans/v1', {
       key: this.apikey,
       steamids: `${steamids}`
