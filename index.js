6add2a3565312142929d7cee26b0cb446c52d857
Paul Mathis <pmathis@abor.com>
2017-10-24 08:41:05 -0500

    Fiex get global stats

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 // Refer to https://developer.valvesoftware.com/wiki/Steam_Web_API for more
 // dtailed api documentation
 
-const {truthy, apiRequest} = require('./helpers');
+const { truthy, apiRequest } = require('./helpers');
 
 class SteamApi {
   constructor(key, steamid, appid) {
@@ -12,29 +12,16 @@ class SteamApi {
     };
   }
 
-  // Merge global configs from the constructor with defaults for the method and
+  // Merge global configs from the constructor with any defaults and
   // then overwrite any parameter inputs from the method call
   _mergeConfigs(config, defaults = {}) {
-    const params = {}
+    const params = {};
     Object.assign(params, this.globalConfig, defaults, config);
     return params;
   }
 
-  getNewsForApp(config) {
-    const defaults = {
-      count: '3',
-      maxlength: '300'
-    };
-    return apiRequest('http://api.steampowered.com/ISteamNews/GetNewsForApp/v0002', this._mergeConfigs(config, defaults));
-  }
-
-  getGlobalAchievementPercentagesForApp(config) {
-    return apiRequest(`http://api.steampowered.com/ISteamUserStats/GetGlobalAchievementPercentagesForApp/v0002`, this._mergeConfigs(config));
-  }
-
-  // FIXME: Convert to new system
-  getGlobalStatsForGame(appid, names, config = {}) {
-    // Takes in a string or an array and assigns the values to objects named
+  _spreadNameArray(names) {
+    // Takes in a string or an array and assigns the values to an object with keys named
     // name[0], name[1], etc...
     const namesObject = {};
     if (typeof names === 'string') {
@@ -44,32 +31,69 @@ class SteamApi {
         namesObject[`name[${i}]`] = name;
       });
     }
-    // Join the namesObject and the config into a single object that is passed as
-    // parameters
-    return apiRequest('http://api.steampowered.com/ISteamUserStats/GetGlobalStatsForGame/v0001', Object.assign({
-      appid,
-      count: config.count || '1'
-    }, namesObject));
+    return namesObject;
+  }
+
+  getNewsForApp(config) {
+    const defaults = {
+      count: '3',
+      maxlength: '300'
+    };
+    return apiRequest(
+      'http://api.steampowered.com/ISteamNews/GetNewsForApp/v0002',
+      this._mergeConfigs(config, defaults)
+    );
+  }
+
+  getGlobalAchievementPercentagesForApp(config) {
+    return apiRequest(
+      `http://api.steampowered.com/ISteamUserStats/GetGlobalAchievementPercentagesForApp/v0002`,
+      this._mergeConfigs(config)
+    );
+  }
+
+  getGlobalStatsForGame(config = {}) {
+    const defaults = {
+      count: '1'
+    };
+    // Turns array of achievement names into an object that can be passed into the get params
+    const namesObject = this._spreadNameArray(config.names);
+    delete config.names; // Remove the array to prevent conflicts
+    Object.assign(config, namesObject); // Merge names object back into users config
+
+    return apiRequest(
+      'http://api.steampowered.com/ISteamUserStats/GetGlobalStatsForGame/v0001',
+      this._mergeConfigs(config, defaults)
+    );
   }
 
   getPlayerSummaries(config = {}) {
-    config.steamids = `${config.steamids || this.globalConfig.steamid}` // Stringify inputed array or insert global steamid
+    config.steamids = `${config.steamids || this.globalConfig.steamid}`; // Stringify inputed array or insert global steamid
     return apiRequest('http://api.steampowered.com/ISteamUser/GetPlayerSummaries/v002', this._mergeConfigs(config));
   }
 
   getFriendList(config) {
     const defaults = {
       relationship: 'friend'
-    }
-    return apiRequest('http://api.steampowered.com/ISteamUser/GetFriendList/v0001', this._mergeConfigs(config, defaults));
+    };
+    return apiRequest(
+      'http://api.steampowered.com/ISteamUser/GetFriendList/v0001',
+      this._mergeConfigs(config, defaults)
+    );
   }
 
   getPlayerAchievements(config) {
-    return apiRequest('http://api.steampowered.com/ISteamUserStats/GetPlayerAchievements/v0001', this._mergeConfigs(config));
+    return apiRequest(
+      'http://api.steampowered.com/ISteamUserStats/GetPlayerAchievements/v0001',
+      this._mergeConfigs(config)
+    );
   }
 
   getUserStatsForGame(config) {
-    return apiRequest(' http://api.steampowered.com/ISteamUserStats/GetUserStatsForGame/v0002', this._mergeConfigs(config));
+    return apiRequest(
+      ' http://api.steampowered.com/ISteamUserStats/GetUserStatsForGame/v0002',
+      this._mergeConfigs(config)
+    );
   }
 
   getOwnedGames(config = {}) {
@@ -79,19 +103,28 @@ class SteamApi {
     const defaults = {
       include_appinfo: false,
       include_played_free_games: false
-    }
+    };
 
-    return apiRequest('http://api.steampowered.com/IPlayerService/GetOwnedGames/v0001', this._mergeConfigs(config, defaults));
+    return apiRequest(
+      'http://api.steampowered.com/IPlayerService/GetOwnedGames/v0001',
+      this._mergeConfigs(config, defaults)
+    );
   }
 
   getRecentlyPlayedGames(config) {
-    return apiRequest('http://api.steampowered.com/IPlayerService/GetRecentlyPlayedGames/v0001', this._mergeConfigs(config));
+    return apiRequest(
+      'http://api.steampowered.com/IPlayerService/GetRecentlyPlayedGames/v0001',
+      this._mergeConfigs(config)
+    );
   }
 
   isPlayingSharedGame(config = {}) {
     config.appid_playing = config.appid_playing || this.globalConfig.appid;
 
-    return apiRequest('http://api.steampowered.com/IPlayerService/IsPlayingSharedGame/v0001', this._mergeConfigs(config));
+    return apiRequest(
+      'http://api.steampowered.com/IPlayerService/IsPlayingSharedGame/v0001',
+      this._mergeConfigs(config)
+    );
   }
 
   getSchemaForGame(config) {
@@ -99,7 +132,7 @@ class SteamApi {
   }
 
   getPlayerBans(config = {}) {
-    config.steamids = `${config.steamids || this.globalConfig.steamid}` // Stringify inputed array or insert global steamid
+    config.steamids = `${config.steamids || this.globalConfig.steamid}`; // Stringify inputed array or insert global steamid
     return apiRequest('http://api.steampowered.com/ISteamUser/GetPlayerBans/v1', this._mergeConfigs(config));
   }
 }
