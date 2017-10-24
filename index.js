2bfaf7fb98268eda61521ead58ac0844e0b54272
Paul Mathis <paul.mathis@gmail.com>
2017-10-23 21:57:42 -0500

    All methods now accept one config object that is merged in with default and global variables

--- index.js
+++ index.js
@@ -1,32 +1,41 @@
 // Refer to https://developer.valvesoftware.com/wiki/Steam_Web_API for more
 // dtailed api documentation
 
-const { truthy, apiRequest } = require('./helpers');
+const {truthy, apiRequest} = require('./helpers');
 
 class SteamApi {
-  constructor(apikey, steamid, appid) {
-    this.apikey = apikey;
-    this.steamid = steamid;
-    this.appid = appid;
-  }
-
-  getNewsForApp(appid, config = {}) {
-    const params = {
-      appid: config.appid || this.appid,
-      count: config.count || '3',
-      maxlength: config.maxlength || '300'
-    };
-    return apiRequest('http://api.steampowered.com/ISteamNews/GetNewsForApp/v0002', params);
-  }
-
-  getGlobalAchievementPercentagesForApp(appid) {
-    return apiRequest('http://api.steampowered.com/ISteamUserStats/GetGlobalAchievementPercentagesForApp/v0002', {
+  constructor(key, steamid, appid) {
+    this.globalConfig = {
+      key,
+      steamid,
       appid
-    });
+    };
   }
 
+  // Merge global configs from the constructor with defaults for the method and
+  // then overwrite any parameter inputs from the method call
+  _mergeConfigs(config, defaults = {}) {
+    const params = {}
+    Object.assign(params, this.globalConfig, defaults, config);
+    return params;
+  }
+
+  getNewsForApp(config) {
+    const defaults = {
+      count: '3',
+      maxlength: '300'
+    };
+    return apiRequest('http://api.steampowered.com/ISteamNews/GetNewsForApp/v0002', this._mergeConfigs(config, defaults));
+  }
+
+  getGlobalAchievementPercentagesForApp(config) {
+    return apiRequest(`http://api.steampowered.com/ISteamUserStats/GetGlobalAchievementPercentagesForApp/v0002`, this._mergeConfigs(config));
+  }
+
+  // FIXME: Convert to new system
   getGlobalStatsForGame(appid, names, config = {}) {
-    // Takes in a string or an array and assigns the values to objects named name[0], name[1], etc...
+    // Takes in a string or an array and assigns the values to objects named
+    // name[0], name[1], etc...
     const namesObject = {};
     if (typeof names === 'string') {
       namesObject['name[0]'] = names;
@@ -35,91 +44,63 @@ class SteamApi {
         namesObject[`name[${i}]`] = name;
       });
     }
-    // Join the namesObject and the config into a single object that is passed as parameters
-    return apiRequest(
-      'http://api.steampowered.com/ISteamUserStats/GetGlobalStatsForGame/v0001',
-      Object.assign(
-        {
-          appid,
-          count: config.count || '1'
-        },
-        namesObject
-      )
-    );
+    // Join the namesObject and the config into a single object that is passed as
+    // parameters
+    return apiRequest('http://api.steampowered.com/ISteamUserStats/GetGlobalStatsForGame/v0001', Object.assign({
+      appid,
+      count: config.count || '1'
+    }, namesObject));
   }
 
-  getPlayerSummaries(steamids = this.steamid) {
-    return apiRequest('http://api.steampowered.com/ISteamUser/GetPlayerSummaries/v002', {
-      key: this.apikey,
-      steamids: `${steamids}`
-    });
+  getPlayerSummaries(config = {}) {
+    config.steamids = `${config.steamids || this.globalConfig.steamid}` // Stringify inputed array or insert global steamid
+    return apiRequest('http://api.steampowered.com/ISteamUser/GetPlayerSummaries/v002', this._mergeConfigs(config));
   }
 
-  getFriendList(steamid = this.steamid, config = {}) {
-    return apiRequest('http://api.steampowered.com/ISteamUser/GetFriendList/v0001', {
-      key: this.apikey,
-      steamid,
-      relationship: config.relationship || 'friend'
-    });
+  getFriendList(config) {
+    const defaults = {
+      relationship: 'friend'
+    }
+    return apiRequest('http://api.steampowered.com/ISteamUser/GetFriendList/v0001', this._mergeConfigs(config, defaults));
   }
 
-  getPlayerAchievements(steamid = this.steamid, appid, config = {}) {
-    return apiRequest('http://api.steampowered.com/ISteamUserStats/GetPlayerAchievements/v0001', {
-      key: this.apikey,
-      steamid,
-      appid
-    });
+  getPlayerAchievements(config) {
+    return apiRequest('http://api.steampowered.com/ISteamUserStats/GetPlayerAchievements/v0001', this._mergeConfigs(config));
   }
 
-  getUserStatsForGame(steamid = this.steamid) {
-    return apiRequest(' http://api.steampowered.com/ISteamUserStats/GetUserStatsForGame/v0002', {
-      key: this.apikey,
-      steamid,
-      appid
-    });
+  getUserStatsForGame(config) {
+    return apiRequest(' http://api.steampowered.com/ISteamUserStats/GetUserStatsForGame/v0002', this._mergeConfigs(config));
   }
 
   getOwnedGames(config = {}) {
-    const params = {
-      key: this.apikey,
-      steamid: config.steamid || this.steamid,
-      include_appinfo: truthy(config.include_appinfo) || false,
-      include_played_free_games: truthy(config.include_played_free_games) || false
-    };
+    config.include_appinfo = truthy(config.include_appinfo);
+    config.include_played_free_games = truthy(config.include_played_free_games);
 
-    return apiRequest('http://api.steampowered.com/IPlayerService/GetOwnedGames/v0001', params);
+    const defaults = {
+      include_appinfo: false,
+      include_played_free_games: false
+    }
+
+    return apiRequest('http://api.steampowered.com/IPlayerService/GetOwnedGames/v0001', this._mergeConfigs(config, defaults));
   }
 
-  getRecentlyPlayedGames(steamid = this.steamid, config = {}) {
-    return apiRequest('http://api.steampowered.com/IPlayerService/GetRecentlyPlayedGames/v0001', {
-      key: this.apikey,
-      steamid,
-      count: config.count
-    });
+  getRecentlyPlayedGames(config) {
+    return apiRequest('http://api.steampowered.com/IPlayerService/GetRecentlyPlayedGames/v0001', this._mergeConfigs(config));
   }
 
   isPlayingSharedGame(config = {}) {
-    const params = {
-      key: this.apikey,
-      steamid: config.steamid || this.steamid,
-      appid_playing: config.appid_playing
-    };
+    config.appid_playing = config.appid_playing || this.globalConfig.appid;
 
-    return apiRequest('http://api.steampowered.com/IPlayerService/IsPlayingSharedGame/v0001', params);
+    return apiRequest('http://api.steampowered.com/IPlayerService/IsPlayingSharedGame/v0001', this._mergeConfigs(config));
   }
 
-  getSchemaForGame(appid) {
-    return apiRequest('http://api.steampowered.com/ISteamUserStats/GetSchemaForGame/v2', {
-      key: this.apikey,
-      appid
-    });
+  getSchemaForGame(config) {
+    return apiRequest('http://api.steampowered.com/ISteamUserStats/GetSchemaForGame/v2', this._mergeConfigs(config));
   }
 
-  getPlayerBans(steamids = this.steamid) {
-    return apiRequest('http://api.steampowered.com/ISteamUser/GetPlayerBans/v1', {
-      key: this.apikey,
-      steamids: `${steamids}`
-    });
+  getPlayerBans(config = {}) {
+    config.steamids = `${config.steamids || this.globalConfig.steamid}` // Stringify inputed array or insert global steamid
+    return apiRequest('http://api.steampowered.com/ISteamUser/GetPlayerBans/v1', this._mergeConfigs(config));
   }
 }
 
